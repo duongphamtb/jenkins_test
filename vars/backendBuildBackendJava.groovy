bca2e413889a4207fb7f724d6f30f7949c034bfe
Pham <duong.a.pham@capgemini.com>
2021-08-06 17:13:57 +0700

    new way6664dsdsfddfsdfsdd

--- vars/backendBuildBackendJava.groovy
+++ vars/backendBuildBackendJava.groovy
@@ -22,7 +22,7 @@ def call(body) {
             }
             stage('Build') {
                 steps {
-                    echo 'Duong start final11122ddd333...'
+                    echo 'Duong start final11122dddj...'
                 }
             }
             stage('Test') {
