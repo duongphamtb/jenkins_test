3e2aca43ddd3f4ca843c7b9c3a076617eed6219b
Pham <duong.a.pham@capgemini.com>
2021-08-06 13:17:29 +0700

    new way

---
+++ vars/backendBuildBackendJava.groovy
@@ -0,0 +1,43 @@
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    pipeline {
+        agent any
+        triggers {
+            githubPush()
+        }
+        stages {
+            stage('Git checkout') {
+                steps {
+                    git branch: "main",
+                        //credentialsId: "BlueOcean Folder Credentials",
+                        credentialsId: "test2",
+                        url: "git@github.com:duongphamtb/jenkins_test.git"
+                        //url: "https://github.com/duongphamtb/jenkins_test"
+                }
+            }
+            stage('Build') {
+                steps {
+                    echo 'Test sduonggg...'
+                }
+            }
+            stage('Test') {
+                steps {
+                    echo 'Testing..'
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    echo 'Deploying....'
+                }
+            }
+        }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
