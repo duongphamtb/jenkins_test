2b2896e7b4c93d4ffb36dc2bed6ec37fe39165fa
Pham <duong.a.pham@capgemini.com>
2021-08-06 15:10:42 +0700

    new way3

--- vars/backendBuildBackendJava.groovy
+++ vars/backendBuildBackendJava.groovy
@@ -20,7 +20,7 @@ def call(body) {
             }
             stage('Build') {
                 steps {
-                    echo 'Duong start testing111...'
+                    echo 'Duong start final...'
                 }
             }
             stage('Test') {
