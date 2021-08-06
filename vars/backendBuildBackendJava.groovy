11aa7229a58b1efd7ed16bae69af537846e494d9
Pham <duong.a.pham@capgemini.com>
2021-08-06 15:29:57 +0700

    new way6664dsdsd

--- vars/backendBuildBackendJava.groovy
+++ vars/backendBuildBackendJava.groovy
@@ -20,7 +20,7 @@ def call(body) {
             }
             stage('Build') {
                 steps {
-                    echo 'Duong start final111...'
+                    echo 'Duong start final11122...'
                 }
             }
             stage('Test') {
