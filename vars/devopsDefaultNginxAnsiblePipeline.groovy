f7429e9da0da25ac8e0b9d3025a779b13a087569
Pham <duong.a.pham@capgemini.com>
2021-08-08 16:39:32 +0700

    ansible version - update method name1

--- vars/devopsDefaultNginxAnsiblePipeline.groovy
+++ vars/devopsDefaultNginxAnsiblePipeline.groovy
@@ -57,9 +57,9 @@ def call(body) {
                             ansiblePlaybook(
                                                 // credentialsId: "${env.ANSIBLE_SSH_CREDENTIALS_ID}",
                                                 // vaultCredentialsId: """${env.ANSIBLE_VAULT}""",
-                                                inventory: 'ansible/inventories/hosts.yaml',
+                                                inventory: '../ansible/inventories/hosts.yaml',
                                                 dynamicInventory: true,
-                                                playbook: """ansible/playbooks/${config.ANSIBLE_PLAYBOOK}""",
+                                                playbook: """../ansible/playbooks/${config.ANSIBLE_PLAYBOOK}""",
                                                 tags: """${ANSIBLE_TAGS}""",
                                                 colorized: true
                                             )
